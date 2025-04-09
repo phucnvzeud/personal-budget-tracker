ca02f5993cca4f179fef3e8c74a11f3af52c83d6
phucnvzeud <phucnv.zeud@gmail.com>
2025-04-09 14:04:55 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle all other requests by serving the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}); 
